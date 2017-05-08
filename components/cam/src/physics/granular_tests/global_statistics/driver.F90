1ab8d68517b1cbb4aaa418a64ffea71e255be101
Hui Wan <Hui.Wan@pnnl.gov>
2017-05-08 13:46:01 -0700

    cosmetic changes

--- components/cam/src/physics/granular_tests/global_statistics/driver.F90
+++ components/cam/src/physics/granular_tests/global_statistics/driver.F90
@@ -1,12 +1,11 @@
   Program TestEnergyFixer
 
-    use shr_kind_mod,  only: r8=>shr_kind_r8
-    use physpkg,       only: phys_init,phys_final
-    use physics_types, only: physics_state, physics_ptend,physics_tend
-  
+    use shr_kind_mod, only: r8=>shr_kind_r8
+    use ppgrid,       only: begchunk, endchunk
+    use physics_types,only: physics_state, physics_tend
+    use physpkg,      only: phys_init,phys_final
     use constituents, only: cnst_add
-    use physconst, only: mwdry, cpair, mwh2o, cpwv
-    use ppgrid, only : begchunk, endchunk
+    use physconst,    only: mwdry, cpair, mwh2o, cpwv
 
     use global_statistics, only: tp_statistics, get_chunk_stat, get_domain_stat, &
                                  LESS_THAN, GREATER_THAN
@@ -15,13 +14,12 @@
 
     integer :: idummy, mm, ilev, nchnk
 
-    type(physics_state), pointer       :: phys_state(:)
-    type(physics_tend ), pointer       :: phys_tend(:)   
+    type(physics_state), pointer :: phys_state(:) => null()
+    type(physics_tend ), pointer :: phys_tend(:)  => null()
 
-   !type(tp_statistics)          :: global_stat
-   !type(tp_statistics)          :: domain_stat
-    type(tp_statistics), pointer :: chunk_stat(:)
+    type(tp_statistics), pointer :: chunk_stat(:) => null()
     type(tp_statistics)          :: domain_stat
+    type(tp_statistics)          :: global_stat
 
     integer :: ncol = PCOLS-1
     integer :: nstep = STEP
