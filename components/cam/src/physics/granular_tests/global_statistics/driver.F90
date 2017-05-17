20d3b6bf9cb55d9e2e8fb2509a35dbbcf4ecba39
Hui Wan <Hui.Wan@pnnl.gov>
2017-05-17 13:58:59 -0700

    add subroutine to handle multi-level arrays
    and misc. other minor changes.
    
    modified:   driver.F90
    modified:   global_statistics.F90
    modified:   test_maxloc.f90

--- components/cam/src/physics/granular_tests/global_statistics/driver.F90
+++ components/cam/src/physics/granular_tests/global_statistics/driver.F90
@@ -1,7 +1,7 @@
   Program test_global_statistics
 
     use shr_kind_mod, only: r8=>shr_kind_r8
-    use ppgrid,       only: begchunk, endchunk
+    use ppgrid,       only: begchunk, endchunk, pver
     use physics_types,only: physics_state, physics_tend
     use physpkg,      only: phys_init,phys_final
     use constituents, only: cnst_add, cnst_name
@@ -14,27 +14,32 @@
 
     implicit none
 
-    integer :: idummy, mm, ilev, icol
+    integer :: idummy, mm, icol
     integer :: itr, istat, icnst
 
-    type(physics_state), pointer :: phys_state(:) => null()
-    type(physics_tend ), pointer :: phys_tend(:)  => null()
+    type(physics_state), pointer :: phys_state(:) => null()     ! shape: (begchunk:endchunk)
+    type(physics_tend ), pointer :: phys_tend(:)  => null()     ! shape: (begchunk:endchunk)
 
-    type(tp_statistics), pointer :: chunk_stat(:,:) => null()
-    type(tp_statistics), pointer :: domain_stat(:)  => null()
+    type(tp_statistics), pointer :: chunk_stat(:,:) => null()   ! shape: (begchunk:endchunk,nfld)
+    type(tp_statistics), pointer :: domain_stat(:)  => null()   ! shape: (nfld)
 
-    integer :: ncol = PCOLS-1  ! deliberately making ncol and pcols different to make sure that the code works in such situations
+    integer :: ncol = PCOLS-1  ! deliberately making ncol and pcols different to make sure 
+                               ! the code works in such situations
     integer :: nstep = STEP
     integer :: nchnk, ichnk
     logical :: l_print_always = .true.
 
-    integer :: nsum_chunk_col      (BCHNK:ECHNK,PCNST)
-    integer :: nsum_domain_col     (PCNST)
+    integer :: n_tot_cnt_in_chunk (BCHNK:ECHNK,PCNST)
+    integer :: n_tot_cnt_in_domain(PCNST)
 
     !----------------------
    
     begchunk = BCHNK
     endchunk = ECHNK
+
+    write(*,*)
+    write(*,*) 'Active domain size: ',pver,' levels, ',ncol,' columns, ',(endchunk-begchunk+1),' chunks.'
+    write(*,*)
 
     ! Initialize tracer indices
 
@@ -42,8 +47,9 @@
     call cnst_add('CLDLIQ', mwdry, cpair, 0._r8,     idummy, longname='Grid box averaged cloud liquid amount')
     call cnst_add('CLDICE', mwdry, cpair, 0._r8,     idummy, longname='Grid box averaged cloud ice amount')
 
-    ! Register fields for calculation global statistics.
-    ! This has to be done before 'call phys_init' 
+    ! Register fields for calculating global statistics.
+    ! This has to be done before 'call phys_init' which allocates memory for 
+    ! chunk_stat and domain_stat.
 
     call add_stat_field('Q','test_part_1',GREATER_THAN,1.E-4_r8)
     call add_stat_field('Q','test_part_2',SMALLER_THAN,1.E-4_r8)
@@ -54,7 +60,7 @@
     call add_stat_field('CLDICE','test_part_1',SMALLER_THAN,5._r8)
     call add_stat_field('CLDICE','test_part_2',GREATER_THAN,5._r8)
 
-    ! Allocate memory for state, tend, and stat vectors
+    ! Allocate memory for state, tend, and stat vectors; read in initial conditions.
 
     call phys_init(phys_state, phys_tend, chunk_stat, domain_stat, nstep)
 
@@ -72,34 +78,31 @@
 
        write(*,*) '-------------'
        write(*,*) 'chunk ',ichnk
-      !write(*,*)
-
-       ilev = 40 
 
        do icnst = 1,PCNST
 
-         nsum_chunk_col(ichnk,icnst) = 0
+         n_tot_cnt_in_chunk(ichnk,icnst) = 0
 
          itr = icnst
          call get_stat_field_idx(cnst_name(icnst),'test_part_1',istat)
-         call get_chunk_stat( ichnk, ncol, phys_state(ichnk)%q(:ncol,ilev,itr),             &! intent(in)
+         call get_chunk_stat( ncol, pver, phys_state(ichnk)%q(:ncol,:,itr),                 &! intent(in)
                               phys_state(ichnk)%lat, phys_state(ichnk)%lon, l_print_always, &! intent(in)
                               chunk_stat(ichnk,istat) )                                      ! intent(inout)
 
-         nsum_chunk_col(ichnk,icnst) = nsum_chunk_col(ichnk,icnst) + chunk_stat(ichnk,istat)%count
+         n_tot_cnt_in_chunk(ichnk,icnst) = n_tot_cnt_in_chunk(ichnk,icnst) + chunk_stat(ichnk,istat)%count
 
          call get_stat_field_idx(cnst_name(icnst),'test_part_2',istat)
-         call get_chunk_stat( ichnk, ncol, phys_state(ichnk)%q(:ncol,ilev,itr),             &! intent(in)
+         call get_chunk_stat( ncol, pver, phys_state(ichnk)%q(:ncol,:,itr),                 &! intent(in)
                               phys_state(ichnk)%lat, phys_state(ichnk)%lon, l_print_always, &! intent(in)
                               chunk_stat(ichnk,istat) )                                      ! intent(inout)
 
-         nsum_chunk_col(ichnk,icnst) = nsum_chunk_col(ichnk,icnst) + chunk_stat(ichnk,istat)%count
+         n_tot_cnt_in_chunk(ichnk,icnst) = n_tot_cnt_in_chunk(ichnk,icnst) + chunk_stat(ichnk,istat)%count
 
        end do
     end do
 
-    if (any(nsum_chunk_col/=ncol)) then
-       write(*,*) nsum_chunk_col
+    if (any(n_tot_cnt_in_chunk/=ncol*pver)) then
+       write(*,*) n_tot_cnt_in_chunk
        call endrun('Test error in chunk_stat.')
     end if
 
@@ -108,27 +111,34 @@
     nchnk = endchunk-begchunk+1
     write(*,*) '-------------'
     write(*,*) 'entire domain '
-   !write(*,*)
 
     do icnst = 1,PCNST
 
-       nsum_domain_col(icnst) = 0
+       n_tot_cnt_in_domain(icnst) = 0
 
        itr = icnst
        call get_stat_field_idx(cnst_name(icnst),'test_part_1',istat)
        call get_domain_stat( l_print_always, nchnk, chunk_stat(:,istat), domain_stat(istat) ) !intent: 3xin, inout
 
-       nsum_domain_col(icnst) = nsum_domain_col(icnst) + domain_stat(istat)%count
+       n_tot_cnt_in_domain(icnst) = n_tot_cnt_in_domain(icnst) + domain_stat(istat)%count
 
        call get_stat_field_idx(cnst_name(icnst),'test_part_2',istat)
        call get_domain_stat( l_print_always, nchnk, chunk_stat(:,istat), domain_stat(istat) ) !intent: 3xin, inout
 
-       nsum_domain_col(icnst) = nsum_domain_col(icnst) + domain_stat(istat)%count
+       n_tot_cnt_in_domain(icnst) = n_tot_cnt_in_domain(icnst) + domain_stat(istat)%count
     end do
 
-    if (any(nsum_domain_col/=ncol*nchnk)) then
+    !----------------------------------
+    ! Check if the results are correct
+    !----------------------------------
+    ! For each constituent, n_tot_cnt_in_domain should match the total number of cells in the domain
+
+    if (any(n_tot_cnt_in_domain/=ncol*nchnk*pver)) then
        call endrun('Test error in domain_stat.')
     end if
+
+    ! For each consitituent and stat_type, the total count for the domain shound match the sum of 
+    ! the counts from all chunks.
 
     if (any(domain_stat(:)%count/=sum(chunk_stat(:,:)%count,dim=1))) then
        write(*,*) domain_stat(:)%count
@@ -140,8 +150,8 @@
     print*, 'Test finished without error.'
     print*, '============================'
 
-   !Below we have an intended error. The code should stop in ENDRUN.
-   !call get_stat_field_idx('U',istat)
-   !call get_domain_stat( l_print_always, nchnk, chunk_stat(:,istat), domain_stat(istat) ) !intent: 3xin, inout
+   !Below are intended errors. The code should stop in ENDRUN.
+   !call get_stat_field_idx('Q','test_part_3',istat)
+   !call get_stat_field_idx('U','test_part_1',istat)
 
   end Program test_global_statistics
--- components/cam/src/physics/granular_tests/global_statistics/test_maxloc.f90
+++ components/cam/src/physics/granular_tests/global_statistics/test_maxloc.f90
@@ -1,10 +1,17 @@
 program main
 
-  real :: array(3)
-  integer :: icol(1)
+  real :: array_1d(3)
+  real :: array_2d(3,2)
+  integer :: icol_1d(1)
+  integer :: icol_2d(2)
 
-  array = (/1.,2.,3./)
-  icol = maxloc( abs(array) )
+  array_1d = (/1.,2.,3./)
+  array_2d(:,1) = (/1.,2.,3./)
+  array_2d(:,2) = (/4.,5.,6./)
 
-  print*,  icol
+  icol_1d = maxloc( abs(array_1d) )
+  icol_2d = maxloc( abs(array_2d) )
+
+  print*,  icol_1d, array_1d(icol_1d(1))
+  print*,  icol_2d, array_2d(icol_2d(1),icol_2d(2))
 end
